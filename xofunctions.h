03f72024989082f65428839645dc0be5aa6e0fba
AmrMAbdulzaher <amrnassareng@gmail.com>
2023-10-09 21:59:58 +0300

    updating

--- xofunctions.h
+++ xofunctions.h
@@ -1,5 +1,14 @@
+#ifndef xofunctions_h
+#define xonfunctions_h
+
+#ifdef _WIN32
+#define CLEAR "cls"
+#else
+#define CLEAR "clear"
+#endif
+
 void field();
 void input();
 char WinDtcr();
 
-
+#endif
